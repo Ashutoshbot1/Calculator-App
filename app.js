2b73a4c3a8c54c3af8181484b23c814fa738b8d8
Ashutoshbot1 <singhashutoshsa2126@gmail.com>
2023-07-23 12:34:03 +0530

    Calculator App

---
+++ app.js
@@ -0,0 +1,37 @@
+const input=document.getElementById("input");
+
+const buttons=document.querySelectorAll("button");
+
+string="";
+
+buttons.forEach(
+    (button)=>{
+        button.addEventListener("click",
+            (event)=>{
+                let key=event.target.innerHTML;
+                
+                if(key==="="){
+                    string=eval(string);
+                    input.value=string;
+                    // key="";
+                }
+                else if(key==="AC"){
+                    string='';
+                    input.value=string;
+                }
+                else if(key==="DEL"){
+
+                    string=string.substring(0,string.length-1);
+                    input.value=string;
+
+                }else{
+
+                    string+=key;
+                    input.value=string;
+                }
+
+
+            }
+        )
+    }
+)
